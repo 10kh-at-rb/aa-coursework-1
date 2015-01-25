625f64ca4efb3be352ea5edd5d72d549bcadba5d
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-01-25 11:06:28 -0500

    Break out functionality in run method to separate explore_current_words method, add build_path method, add display of word chain from source to target

--- word_chains.rb
+++ word_chains.rb
@@ -31,21 +31,38 @@ class WordChainer
 
   def run(source, target)
     @current_words = [source]
-    @all_seen_words = [source]
-    until @current_words.empty?
+    @all_seen_words = { source => nil }
+    until @current_words.empty? || @all_seen_words.include?(target)
       new_current_words = []
-      @current_words.each do |word|
-        new_adjacent_words = adjacent_words(word)
-        new_adjacent_words.each do |adjacent_word|
-          if !@all_seen_words.include?(adjacent_word)
-            new_current_words << adjacent_word
-            @all_seen_words << adjacent_word
-          end
-        end
-      end
-      new_current_words.each { |word| puts word }
-      @current_words = new_current_words
+      explore_current_words(new_current_words)
+    end
+    word_chain = build_path(target)
+    if word_chain.nil?
+      puts "No path found!"
+    else
+      word_chain.reverse.each { |word| puts word }
     end
   end
 
+  def explore_current_words(new_current_words)
+    @current_words.each do |word|
+      new_adjacent_words = adjacent_words(word)
+      new_adjacent_words.each do |adjacent_word|
+        if !@all_seen_words.include?(adjacent_word)
+          new_current_words << adjacent_word
+          @all_seen_words[adjacent_word] = word
+        end
+      end
+    end
+    @current_words = new_current_words
+  end
+
+  def build_path(target)
+    current_word = target
+    path = [current_word] unless @all_seen_words[current_word].nil?
+    until @all_seen_words[current_word].nil?
+      path << current_word = @all_seen_words[current_word]
+    end
+    path
+  end
 end
