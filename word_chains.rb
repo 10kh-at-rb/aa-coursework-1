da6c78db5252e608c3e414033f15eb9029d132c8
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-01-25 10:14:40 -0500

    Add adjacent words method

--- word_chains.rb
+++ word_chains.rb
@@ -1,11 +1,32 @@
+require 'set'
+
 class WordChainer
 
   def initialize(dictionary_file_name)
     @dictionary = File.readlines(dictionary_file_name).map(&:chomp)
+    @dictionary = Set.new(@dictionary)
   end
 
   def dictionary
     @dictionary
+  end
+
+  def adjacent_words(word)
+    return_dictionary = @dictionary.dup
+    return_dictionary.delete_if do |comparison_word|
+      comparison_word.length != word.length
+    end
+
+    return_dictionary.delete_if do |comparison_word|
+      count = 0
+      0.upto(word.length) do |char|
+        count += 1 if word[char] != comparison_word[char]
+      end
+
+      count != 1
+    end
+
+    return_dictionary
   end
 
 end
