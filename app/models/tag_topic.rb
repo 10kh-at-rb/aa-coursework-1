f5d16927586b01d284b14e62a346d8e46cb18f99
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-02-04 16:46:48 -0500

    Add most popular link method to TagTopic model

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -9,4 +9,10 @@ class TagTopic < ActiveRecord::Base
   )
 
   has_many(:urls, through: :taggings, source: :short_url)
+
+  def popular_links
+    popular_links = self.urls.joins(:visit).group("shortened_urls.id").order('count(visits.user_id) desc').count("visits.user_id")
+    most_popular_link = popular_links.first.first
+    ShortenedURL.find(most_popular_link)
+  end
 end
