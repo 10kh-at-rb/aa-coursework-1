f7eeff03e6b32ed75f7b180585920bbd0bd925fb
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-02-05 10:26:34 -0500

    Create models with basic associations

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ActiveRecord::Base
+  has_many(
+    :authored_polls,
+    class_name: "Poll",
+    foreign_key: :author,
+    primary_key: :id
+  )
+
+  has_many(
+    :responses,
+    class_name: "Response",
+    foreign_key: :user_id,
+    primary_key: :id
+  )
+
+end
