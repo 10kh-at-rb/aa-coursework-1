bc18e611604394d57f617d7dc0cf7ab138a0abf1
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-02-10 11:43:34 -0500

    Add cat model.

---
+++ app/models/cat.rb
@@ -0,0 +1,21 @@
+class Cat < ActiveRecord::Base
+  validates :birth_date, :color, :name, :sex, :description, presence: :true
+  validates :sex, inclusion: { in: ["M", "F"], message: "Sex must be 'M' or 'F'" }
+  validates :color, inclusion: { in: ['black', 'white', 'brown', 'orange'], message: "Color must be a valid color." }
+
+  def age
+    age = Date.today.year - self.birth_date.year
+    months = Date.today.month - self.birth_date.month
+    days = Date.today.day - self.birth_date.day
+    if months < 0
+      age -= 1
+    elsif months == 0
+      if days < 0
+        age -= 1
+      end
+    end
+
+    age
+  end
+
+end
