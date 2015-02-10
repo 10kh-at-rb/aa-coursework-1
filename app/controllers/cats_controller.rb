2ed8d330b0dc5dff01a23a6f0733b7abfe7f5610
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-02-10 14:16:20 -0500

    Add new and edit views

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -12,5 +12,26 @@ class CatsController < ApplicationController
     render :show
   end
 
+  def new
+    @cat = Cat.new
+
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+    if @cat.save
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def cat_params
+    params.require(:cat)
+      .permit(:birth_date, :name, :color, :sex, :description)
+  end
 
 end
