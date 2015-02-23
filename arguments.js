cb89837cc38fc4eb64506436c48bd941cdf804f8
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-02-23 10:44:59 -0500

    Complete curry and curriedSum

--- arguments.js
+++ arguments.js
@@ -17,3 +17,35 @@ Function.prototype.myBindWithArgs = function (obj) {
     return context.apply(obj, arr);
   };
 };
+
+
+var curriedSum = function(numArgs) {
+  var numbers = [];
+  var sum = 0;
+  var _curriedSum = function (num) {
+    numbers.push(num);
+    if (numbers.length === numArgs) {
+      numbers.forEach(function (el) {
+        sum += el;
+      });
+      return sum;
+    }else{
+      return _curriedSum;
+    }
+  };
+  return _curriedSum;
+};
+
+Function.prototype.curry = function(numArgs){
+  var context = this;
+  var numbers = [];
+  var _curried = function(num){
+    numbers.push(num);
+    if (numbers.length === numArgs){
+      return context.apply(null, numbers);
+    } else {
+      return _curried;
+    }
+  };
+  return _curried;
+}
