169515752cffbe50186ebc3bf322edd8cc9c1115
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-03-02 12:07:35 -0500

    Add refreshPokemon and addPokemonToList methods

--- app/assets/javascripts/pokedex-1A.js
+++ app/assets/javascripts/pokedex-1A.js
@@ -1,5 +1,17 @@
 Pokedex.RootView.prototype.addPokemonToList = function (pokemon) {
+  $li = $("<li></li>");
+  $li.text( pokemon.get("name") + " " + pokemon.get("poke_type"));
+  this.$pokeList.append($li);
+  $li.addClass("poke-list-item");
 };
 
 Pokedex.RootView.prototype.refreshPokemon = function (callback) {
+  this.pokes.fetch({
+    success: function() {
+      _.each(this.pokes.models, function(pokemon) {
+        this.addPokemonToList(pokemon);
+      }, this);
+    }.bind(this)
+  });
+
 };
